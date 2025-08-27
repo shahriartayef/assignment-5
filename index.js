fdb5b1b55454d62ea3d56143b9cb19e4d4b16932
shahriartayef <tayef.ta123@gmail.com>
2025-08-28 00:15:45 +0600

    starting

---
+++ index.js
@@ -0,0 +1,25 @@
+
+let coins = 100;
+let availableCopy=0;
+let availableHeart=0;
+//function to get input value
+
+/*function getInputValueNumber(id){
+    const inputField=document.getElementById(id)
+    const inputFieldValue= inputField.value
+    //const inputFieldValueNumber= parseint(inputFieldValue)
+}
+function getInputValue(id){
+    const inputField=document.getElementById(id)
+    const inputFieldValue= inputField.value
+    return inputFieldValue
+}*/
+//function to get innertext
+
+/*function getInnerText(id){
+    //const element=document.getElementById(available-coin)
+    //const elementValue= element.Value
+    //const elementValueNumber= parseint(elementValue)
+*/
+
+//function to set innertext
